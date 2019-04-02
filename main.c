e59a692fdda67a2a43ef42d540122215882b7726
Bella Sherring <isabella.sherring@uon.edu.au>
2019-04-02 02:06:06 +0000

    might work

--- main.c
+++ main.c
@@ -6,13 +6,16 @@ char encryption(char *word, int N); //func prototype
 
 int main()
 {
+    int N=7;
     char word[20]={'b', 'r', 'e', 'a', 'k', '\0'}; //string is break
-    printf("%c\n", word);
+    printf("%s\n", word);
     return 0;
 }
 
 char encryption(char *word, int N) //func definition
 {
-    int encryption;
-    int word;
+    int encoded;
+    int term=0;
+    encoded=term;
+    return encoded;
 }
