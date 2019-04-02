39aec1f6ae0d4d0457780202290421258268712d
Bella Sherring <isabella.sherring@uon.edu.au>
2019-04-02 02:43:16 +0000

    this is actually working wtffffff

--- main.c
+++ main.c
@@ -2,20 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-char encryption(char *word, int N); //func prototype
+char encrypt(char *word); //function prototype that will eventually encrypt something
 
 int main()
 {
-    int N=7;
-    char word[20]={'b', 'r', 'e', 'a', 'k', '\0'}; //string is break
-    printf("%s\n", word);
+    char encoded;
+    char word[20]="HELLO"; //initialises string as break
+    printf("%s\n", word); //prints the string before its encoded
+    
+    //scanf for key here so user can choose encryption shifting value?
+    
+    //encoding function 
+    encoded=encrypt(word);
+    
     return 0;
 }
 
-char encryption(char *word, int N) //func definition
+char encrypt(char *word) //function definition which doesnt do anything yet bc idk how
 {
-    int encoded;
-    int term=0;
-    encoded=term;
-    return encoded;
+    char i=0;
+    int key=11; //11 is the max to add, before characters are used instead of letters
+    while(word[i] > 20)
+    {
+        word[i]=word[i]+key;
+        printf("%c", word[i]);
+        i++;
+    }
 }
