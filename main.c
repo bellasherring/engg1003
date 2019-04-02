887fbc10212050a897bb66433d198faa140222bc
Bella Sherring <isabella.sherring@uon.edu.au>
2019-04-02 01:14:32 +0000

    another one

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    printf("Hello wOrLd");
+    int x;
+    int w;
+    printf("this should print before you enter a number \n");
+    scanf("%d", &x);
+    printf("the number you entered is %d\n", x);
+    scanf("%d", &w);
+    printf("the other number is %d\n", w);
     return 0;
 }
